9df5b69407b021e80587fc48c2b4f6a0314bfdc8
Z002Z9 <ujhelyi.gergo2003@gmail.com>
2026-01-03 21:10:11 +0100

    simple encryption works

--- encryptionbyme/encryption.cpp
+++ encryptionbyme/encryption.cpp
@@ -3,8 +3,10 @@
 #include <cctype>
 #include <iostream>
 #include <list>
+#include <vector>
 
 using namespace std;
+//updated
 
 
 //bool performCeaserChiper(string& content, bool encryption)
@@ -22,82 +24,111 @@ using namespace std;
 bool Hillencryption(string& content, bool encryption)
 {
 
-	bool encrypt = encryption ? true : false;
-    int matrix[3][3] = 
+
+    int matrix[3][3] =
     {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9} 
+        {17, 17, 5},
+        {21, 18, 21},
+        {2, 2, 19}
     };
-
-    list<char> letters;
-
-    for (char& c : content)
+    if (!encryption)
     {
-        if (isalpha(c) || c == ' ' || c == '.' || c == ',')
+
+
+        list<char> letters;
+
+        for (char& c : content)
         {
-            if (isupper(c)) {
-            c = tolower(c);  
+            if (isalpha(c))// || c == ' ' || c == '.' || c == ','
+            {
+                if (isupper(c))
+                {
+                    c = tolower(c);
+                }
+                letters.push_back(c);
+
+
             }
-			letters.push_back(c);
-            
-
         }
-    }
-    if (letters.size() % 3 == 1)
-    {
-        letters.push_back('#');
-        letters.push_back('#');
-    }
-    else if (letters.size() % 3 == 2)
-    {
-        letters.push_back('#');
-    }
-
-    for (char& c : letters)
-    {
-        cout << c;
-    }
-
-    cout << endl;
-
-    list<int> chartonumber;
-
-    for (char& c : letters)
-    {
-		if (c == ' ')
-		{
-			c = '26';
-		}
-		else if (c == '.')
-		{
-			c = '27';
-		}
-		else if (c == ',')
-		{
-			c = '28';
-		}
-        else
+        if (letters.size() % 3 == 1)
         {
-			int tmp = (int)c - 97;
-            chartonumber.push_back(tmp);
+            letters.push_back('#');
+            letters.push_back('#');
         }
-        
+        else if (letters.size() % 3 == 2)
+        {
+            letters.push_back('#');
+        }
+
+        /*for (char& c : letters)
+        {
+            cout << c;
+        }*/
+
+        cout << endl;
+
+        list<int> chartonumber;
+
+        for (char& c : letters)
+        {
+            /*if (c == ' ')
+            {
+                c = '26';
+            }
+            else if (c == '.')
+            {
+                c = '27';
+            }
+            else if (c == ',')
+            {
+                c = '28';
+            }
+            else
+            {
+                int tmp = (int)c - 97;
+                chartonumber.push_back(tmp);
+            }*/
+            int tmp = (int)c - 97;
+            chartonumber.push_back(tmp);
+
+        }
+
+
+        list<int> matrixtimes;
+        vector<int> convtovector(chartonumber.begin(), chartonumber.end());
+
+
+        //for (int i = 0; i < chartonumber.size()/3-1; i+=3)
+        for (int i = 0; i < (int)chartonumber.size(); i += 3)
+        {
+            int tmp1 = convtovector[i] * matrix[0][0] + convtovector[i + 1] * matrix[1][0] + convtovector[i + 2] * matrix[2][0];
+            tmp1 = tmp1 % 26;
+            int tmp2 = convtovector[i] * matrix[0][1] + convtovector[i + 1] * matrix[1][1] + convtovector[i + 2] * matrix[2][1];
+            tmp2 = tmp2 % 26;
+            int tmp3 = convtovector[i] * matrix[0][2] + convtovector[i + 1] * matrix[1][2] + convtovector[i + 2] * matrix[2][2];
+            tmp3 = tmp3 % 26;
+
+            matrixtimes.push_back(tmp1);
+            matrixtimes.push_back(tmp2);
+            matrixtimes.push_back(tmp3);
+
+        }
+
+        content.clear();
+        char mychar = 'n';
+        for (int n : matrixtimes)
+        {
+            mychar = n + 'a';
+
+            content += mychar;
+        }
     }
-    for (int& c : chartonumber)
+    else
     {
-        cout << c <<endl;
+        //decript
     }
 
-    list<int> matrixtimes;
-
-    for (int i = 0; i < chartonumber.size() / 3; i++)
-    {
-		chartonumber(0) * matrix[0][0] + chartonumber[i + 1] * matrix[0][1] + chartonumber[i + 2] * matrix[0][2];
-
-    }
-
-
+    
 
 	return true;
 
@@ -116,14 +147,14 @@ bool encryptFile(const string& filename, bool encrypt) {
 
     if (Hillencryption(content, encrypt))
     {
-        /*ofstream outFile(encrypt ? "encrypted_" + filename : "decrypted_" + filename);
+        ofstream outFile(encrypt ? "encrypted_" + filename : "decrypted_" + filename);
         if (!outFile)
         {
             return false;
         }
         outFile << content;
         cout << content << endl;
-        outFile.close();*/
+        outFile.close();
         return true;
     }
 }
